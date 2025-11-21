9a89ff9e0d2e3f42538d8406d468995b77f23b21
Jason Vivek Nicholas <jasonnicky2003@gmail.com>
2025-11-21 10:31:22 +0530

    Second commit of BankEncapsulation Project on GitHub. Updated the deposit and withdrawal methods and changed the print statements in all 3 classes.

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -2,18 +2,18 @@ public class SavingsAccount extends BankAccount {
 
     @Override
     public double deposit(double amount){
-        System.out.println(" Money is being Deposited to Savings Account: ");
-        setBalance(getBalance() + amount);
 
+        System.out.printf("\n₹ %.2f is being Deposited to Savings Account: ",amount);
+        setBalance(getBalance() + amount);
         return getBalance();
     }
 
     @Override
     public double withdrawal(double amount){
-        System.out.println("Money is being Withdrawn from Savings Account: ");
+        System.out.printf("\n₹ %.2f is being Withdrawn from Savings Account: ",amount);
 
         if(amount > getBalance()){
-            System.out.println("Savings are low, CANNOT Withdraw Money from Savings Account."+getBalance());
+            System.out.println("Savings are low, CANNOT Withdraw Money from Savings Account. \nCurrent Balance: "+getBalance());
             return getBalance();
         }
 
