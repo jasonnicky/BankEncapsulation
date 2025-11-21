5261d7511a6b02aeed799d9d5b6d986faf2c4455
Jason Vivek Nicholas <jasonnicky2003@gmail.com>
2025-11-21 11:19:52 +0530

    Second commit of BankEncapsulation Project on GitHub in Branch-V1-w/o-Scanner-class Updated all 4 classes.

--- src/BankEncapsulation.java
+++ src/BankEncapsulation.java
@@ -3,22 +3,44 @@ import java.util.Scanner;
 public class BankEncapsulation {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         // SavingsAccount details
         SavingsAccount sa = new SavingsAccount();
 
         sa.setAccountName("Rob-B01");
         sa.setAccountHolderName("Robert");
-        sa.deposit(15000.00);
-        sa.withdrawal(5000.00);
-        System.out.println("\nFinal Balance in Savings Account: "+sa.getBalance());
+        /*sa.deposit(15000.00);
+        sa.withdrawal(5000.00);*/
+
+        System.out.print("Enter the amount you want to deposit initially into your Savings Account: ");
+        double sa_deposit = sc.nextDouble();
+        sa.deposit(sa_deposit);
+
+        System.out.println();
+        System.out.print("\nEnter the amount you want to withdraw from your Savings Account: ");
+        double sa_withdrawal = sc.nextDouble();
+        sa.withdrawal(sa_withdrawal);
+
+        System.out.printf("\nFinal Balance in Savings Account: ₹ "+sa.getBalance());
 
         // CurrentAccount details
         CurrentAccount ca = new CurrentAccount();
 
         ca.setAccountName("Bob-B02");
         ca.setAccountHolderName("Bobby");
-        ca.deposit(25000.00);
-        ca.withdrawal(5000.00);
-        System.out.println("\nFinal Balance in Current Account: "+ca.getBalance());
+        /*ca.deposit(25000.00);
+        ca.withdrawal(5000.00);*/
+
+        System.out.println();
+        System.out.print("\nEnter the amount you want to deposit initially into your Current Account: ");
+        double ca_deposit = sc.nextDouble();
+        ca.deposit(ca_deposit);
+
+        System.out.println();
+        System.out.print("\nEnter the amount you want to withdraw from your Current Account: ");
+        double ca_withdrawal = sc.nextDouble();
+        ca.withdrawal(ca_withdrawal);
+
+        System.out.println("\nFinal Balance in Current Account: ₹ "+ca.getBalance());
     }
 }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -3,19 +3,20 @@ public class SavingsAccount extends BankAccount {
     @Override
     public double deposit(double amount){
 
-        System.out.printf("\n₹ %.2f is being Deposited to Savings Account: ",amount);
+        System.out.printf("\n₹ %.2f is being Deposited to Savings Account.",amount);
         setBalance(getBalance() + amount);
+        System.out.print("\nCurrent Balance after depositing: ₹ "+getBalance());
         return getBalance();
     }
 
     @Override
     public double withdrawal(double amount){
-        System.out.printf("\n₹ %.2f is being Withdrawn from Savings Account: ",amount);
-
         if(amount > getBalance()){
-            System.out.println("Savings are low, CANNOT Withdraw Money from Savings Account. \nCurrent Balance: "+getBalance());
+            System.out.print("\nSavings are low, CANNOT Withdraw Money from Savings Account. \nCurrent Balance: ₹ "+getBalance());
             return getBalance();
         }
+
+        System.out.printf("\n₹ %.2f is being Withdrawn from Savings Account.",amount);
 
         setBalance(getBalance() - amount);
         return getBalance();
